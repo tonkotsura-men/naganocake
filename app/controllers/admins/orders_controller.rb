46b670a3457f7d413b98e09e9fa53ca91159a6b7
hiroakihonda19910302 <hiroaki.honda3918@gmail.com>
2020-09-24 23:46:45 +0900

    注文ステータス、請求価格修正

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -13,8 +13,17 @@ class Admins::OrdersController < ApplicationController
 
   def update
   	@order = Order.find(params[:id])
+    @order_details = @order.order_details
   	if @order.update(order_params)
-  		redirect_to admins_order_path(@order)	
+       redirect_to admins_orders_path
+        @order_details.each do |order_detail|
+        if @order.status == "入金確認"
+            order_detail.update(making_status: "製作待ち")
+        elsif
+          @order.status == "発送済み"
+          @order.update(status: "発送済み")
+        end
+      end
   	else
   		render "admins/orders/show"
   	end
