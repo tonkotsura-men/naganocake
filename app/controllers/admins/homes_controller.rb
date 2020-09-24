3a947ff5fafaa443383735fe47ea6d4577ac9592
vagrant <vagrant@localhost.localdomain>
2020-09-24 04:22:43 +0000

    TopTodaysOrder reformed

--- app/controllers/admins/homes_controller.rb
+++ app/controllers/admins/homes_controller.rb
@@ -1,6 +1,10 @@
 class Admins::HomesController < ApplicationController
-  def top
-  	today = Date.today
-  	@orders = Order.where("created_at > #{today}")
-  end
+
+def top
+	today = Date.today
+	@orders = Order.where(created_at: today.all_day)
+end
+
+
+
 end
